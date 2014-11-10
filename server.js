52f21ca40554b0877a51c4a29325482b17dc6ac6
Rob Polak <rpolak@godaddy.com>
2014-11-10 17:03:17 -0500

    incremental checkin

--- server.js
+++ server.js
@@ -13,9 +13,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
